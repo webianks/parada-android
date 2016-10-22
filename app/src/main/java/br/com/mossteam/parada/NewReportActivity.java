5bfddc59895fad35c01f2e68078ae08b4a68407d
webianks <webianks@gmail.com>
2016-10-22 14:20:57 +0530

    Added_Runtime_Permissons_And_Fixed_Crash

--- app/src/main/java/br/com/mossteam/parada/NewReportActivity.java
+++ app/src/main/java/br/com/mossteam/parada/NewReportActivity.java
@@ -1,14 +1,21 @@
 package br.com.mossteam.parada;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -16,33 +23,42 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import com.couchbase.lite.Document;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 
-public class NewReportActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class NewReportActivity extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private static final int REQUEST_IMAGE_CAPTURE = 100;
     private GoogleMap mMap;
     LocationManager mLocationManager;
     Location mLocation;
 
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    final int MY_PERMISSIONS_REQUEST_LOCATION = 5000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_report);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.title_activity_new_report);
         setSupportActionBar(toolbar);
@@ -60,21 +76,31 @@ public class NewReportActivity extends AppCompatActivity implements OnMapReadyCa
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView textView1 = (TextView) findViewById(R.id.bus_code);
-                HashMap<String, Object> hashMap = new HashMap<String, Object>();
-                HashMap<String, Object> hashMap2 = new HashMap<String, Object>();
 
-                hashMap.put("bus_code", textView1.getText().toString());
-                hashMap2.put("latitude", String.valueOf(mLocation.getLatitude()));
-                hashMap2.put("longitude", String.valueOf(mLocation.getLongitude()));
-                hashMap2.put("altitude", String.valueOf(mLocation.getAltitude()));
-                hashMap.put("location", hashMap2);
-                hashMap.put("date", new Date().getTime());
+                if (mLocation != null) {
 
-                SyncManager s = new SyncManager(NewReportActivity.this);
-                Document document = s.createDocument();
-                s.updateDocument(document, hashMap);
-                onBackPressed();
+                    TextView textView1 = (TextView) findViewById(R.id.bus_code);
+                    HashMap<String, Object> hashMap = new HashMap<String, Object>();
+                    HashMap<String, Object> hashMap2 = new HashMap<String, Object>();
+
+
+                    hashMap.put("bus_code", textView1.getText().toString());
+                    hashMap2.put("latitude", String.valueOf(mLocation.getLatitude()));
+                    hashMap2.put("longitude", String.valueOf(mLocation.getLongitude()));
+                    hashMap2.put("altitude", String.valueOf(mLocation.getAltitude()));
+                    hashMap.put("location", hashMap2);
+                    hashMap.put("date", new Date().getTime());
+
+
+                    SyncManager s = new SyncManager(NewReportActivity.this);
+                    Document document = s.createDocument();
+                    s.updateDocument(document, hashMap);
+                    onBackPressed();
+
+                } else {
+                    Toast.makeText(NewReportActivity.this, "Location not ready.", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
@@ -82,6 +108,26 @@ public class NewReportActivity extends AppCompatActivity implements OnMapReadyCa
                 getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        getLocation();
+
+    }
+
+    private void getLocation() {
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                // The next two lines tell the new client that “this” current class will handle connection stuff
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                //fourth line adds the LocationServices API endpoint from GooglePlayServices
+                .addApi(LocationServices.API)
+                .enableAutoManage(this, this)
+                .build();
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
     }
 
     @Override
@@ -140,4 +186,175 @@ public class NewReportActivity extends AppCompatActivity implements OnMapReadyCa
                 new LatLng(mLocation.getLatitude(), mLocation
                         .getLongitude()), 18f));
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception ex) {
+        }
+
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (Exception ex) {
+        }
+
+        if (!gps_enabled && !network_enabled) {
+            // notify user
+            final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setTitle("Location");
+            dialog.setMessage(getResources().getString(R.string.gps_network_not_enabled));
+            dialog.setPositiveButton(getResources().getString(R.string.open_location_settings), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+
+                    Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(myIntent);
+                    //get gps
+                }
+            });
+            dialog.setNegativeButton(getString(R.string.Cancel), new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+
+                }
+            });
+            dialog.show();
+        } else {
+
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                            MY_PERMISSIONS_REQUEST_LOCATION);
+                }
+            }
+
+
+        }
+
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                                    != PackageManager.PERMISSION_GRANTED) {
+                       return;
+                    }
+
+                    Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+                    if (location == null) {
+                        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+                    } else {
+                        //If everything went fine lets get latitude and longitude
+                        double currentLatitude = location.getLatitude();
+                        double currentLongitude = location.getLongitude();
+
+                        mLocation = location;
+
+                        Toast.makeText(this, currentLatitude + " WORKS " + currentLongitude + "", Toast.LENGTH_LONG).show();
+
+                    }
+
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+            /*
+             * Google Play services can resolve some errors it detects.
+             * If the error has a resolution, try sending an Intent to
+             * start a Google Play services activity that can resolve
+             * error.
+             */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                    /*
+                     * Thrown if Google Play services canceled the original
+                     * PendingIntent
+                     */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+                /*
+                 * If no resolution is available, display a dialog to the
+                 * user with the error.
+                 */
+            Log.e("Error", "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    /**
+     * If locationChanges change lat and long
+     *
+     * @param location
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        mLocation = location;
+    }
+
+
 }
